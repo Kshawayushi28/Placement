41bfc882af3d0d8eaf8be5495a260bbe6277f5df
Kshawayushi28 <shawayushi110@gmail.com>
2026-02-17 00:04:14 +0530

    first commit

--- KK/02-first-java/Fibonacci.java
+++ KK/02-first-java/Fibonacci.java
@@ -8,20 +8,12 @@ class Fibonacci{
         System.out.print("Enter n: ");
         int n = sc.nextInt();
         int a = 0,b = 1;
-        // if(n==0){
-        //     System.out.println(a);
-        // }else if(n==1){
-        //     System.out.println(b);
-        // }
-        // else{
-            for(int i=1;i<=n;i++){
-                System.out.print(a + " ");
-                int c = a+b;
-                a=b;
-                b=c;
-            }
-            // System.out.println("Fibonacci no. is: "+b);
-        // }
+        for(int i=1;i<=n;i++){
+            System.out.print(a + " ");
+            int c = a+b;
+            a=b;
+            b=c;
+        }
         sc.close();
     }
 }
